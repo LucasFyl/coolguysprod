7f72791e09eb1866a9b0b0f1a05d1f033513ffce
Lucas Fayolle <lfayolle1@gmail.com>
2015-11-09 10:09:10 -0800

    last css fix before 1deploy

---
+++ app/site/templates/contact.php
@@ -0,0 +1,30 @@
+<?php snippet('head') ?>
+
+  <div id="content" class="contact">
+  	<?php snippet('header') ?>
+
+	<div class="fixed-content">
+		<h1 class="logo">Cool Guys Production</h1>
+		<span>get in touch.</span>
+	</div>
+    <div class="yellow-row">
+    	<div class="ten">
+    		<div class="six">
+	    		<p>
+	    			We carry a spirit of finding creative approaches to help our clients 
+	    			realize their visions. Let's build a great relationship over coffee 
+	    			and figure out how we can be of service.
+	    		</p>
+	    	</div>
+	    	<div class="six">
+	    		<a href="https://goo.gl/maps/s8CkeqU52j42" target="_blank">3077 N Park Way #107 San Diego, CA 92104</a>
+	    		<a href="mailto:coolguysinfo@gmail.com">coolguysinfo@gmail.com</a>
+	    		<a href="phone:9092308484">(909)230-8484</a>
+	    	</div>
+    	</div>
+    </div>
+  </div> <!-- / #content  -->
+
+<?php snippet('modal') ?>
+
+<?php snippet('footer') ?>
